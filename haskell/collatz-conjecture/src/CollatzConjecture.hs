e555c83a905604f264b8492c36851eff5b198640
Alex Truslow <atruslow@mercury.com>
2023-12-14 13:07:15 -0500

    Refactoring

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -4,13 +4,11 @@ collatz :: Integer -> Maybe Integer
 collatz 1 = Just 0
 collatz n 
     | n <= 0 = Nothing
-    | otherwise = Just (colltaz_steps 0 n)
+    | otherwise =  Just $ toInteger $ length $ takeWhile (> 1) (iterate colltazStep n)
 
 
-colltaz_steps:: Integer -> Integer -> Integer
-colltaz_steps step_count 0 = step_count
-colltaz_steps step_count 1 = step_count
-colltaz_steps step_count num
-    | num `mod` 2 == 0 = colltaz_steps new_step_count (num `div` 2)
-    | otherwise = colltaz_steps new_step_count ((3 * num) + 1)
-    where new_step_count = step_count + 1
+colltazStep:: Integer -> Integer
+colltazStep 1 = 1
+colltazStep num
+    | num `mod` 2 == 0 = (num `div` 2)
+    | otherwise = (3 * num) + 1
