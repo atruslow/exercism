3afd582857667704532a81b4fd35ac01a1e22d65
Alex Truslow <atruslow@mercury.com>
2024-01-08 10:33:11 -0500

    Coding Practice

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,21 @@
 module DNA (toRNA) where
 
+datagit  DNA =
+    | G
+    | C
+    | T
+    | A
+
+Type RNA =
+    | C
+
+
+
+
+- `G` -> `C`
+- `C` -> `G`
+- `T` -> `A`
+- `A` -> `U`
+
 toRNA :: String -> Either Char String
 toRNA xs = error "You need to implement this function."
--- python/guidos-gorgeous-lasagna/lasagna.py
+++ python/guidos-gorgeous-lasagna/lasagna.py
@@ -3,15 +3,10 @@
 Learn about Guido, the creator of the Python language: https://en.wikipedia.org/wiki/Guido_van_Rossum
 """
 
-# TODO: define the 'EXPECTED_BAKE_TIME' constant
-# TODO: consider defining the 'PREPARATION_TIME' constant
-#       equal to the time it takes to prepare a single layer
-
 EXPECTED_BAKE_TIME = 40
 PREPARATION_TIME = 2
 
 
-# TODO: define the 'bake_time_remaining()' function
 def bake_time_remaining(elapsed_bake_time: int) -> int:
     """Calculate the bake time remaining.
 
@@ -30,6 +25,7 @@ def preparation_time_in_minutes(layers: int) -> int:
     """Did it"""
     return layers * PREPARATION_TIME
 
+
 def elapsed_time_in_minutes(number_of_layers: int, elapsed_bake_time: int) -> int:
     """did it"""
-    return preparation_time_in_minutes(number_of_layers) + bake_time_remaining(elapsed_bake_time)
+    return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
