5b9fe89693cc8a231c85291a6e24182ebec54c01
Alex Truslow <atruslow@mercury.com>
2024-01-25 12:39:07 -0500

    Haskell work

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,21 +1,18 @@
 module DNA (toRNA) where
 
-datagit  DNA =
-    | G
-    | C
-    | T
-    | A
-
-Type RNA =
-    | C
-
-
-
-
-- `G` -> `C`
-- `C` -> `G`
-- `T` -> `A`
-- `A` -> `U`
+mapDNAtoRNA :: Char -> Either Char Char
+mapDNAtoRNA 'G' = Right 'C'
+mapDNAtoRNA 'C' = Right 'G'
+mapDNAtoRNA 'T' = Right 'A'
+mapDNAtoRNA 'A' = Right 'U'
+mapDNAtoRNA x = Left x
 
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA = mapM mapDNAtoRNA
+    
+    
+    
+    
+    -- if all isValidDNA xs
+    -- then Right  (map mapDNAtoRNA xs)
+    -- else Left (find( not (isValidDNA ) ))
