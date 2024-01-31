04e9488450d1462d68600e08815c1fa5eaff9e30
Alex Truslow <atruslow@mercury.com>
2024-01-31 12:57:31 -0500

    Haskell work

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,8 +1,21 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
+import qualified Data.Map as Map
 
-data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
+data Nucleotide = A | C | G | T deriving (Eq, Ord, Show, Read)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts xs = case mapM toNucleotide xs of
+    Just dnaStr -> Right (Map.fromListWith (+) [(x, 1) | x <- dnaStr])
+    Nothing -> Left "Invalid"
+
+toNucleotide :: Char -> Maybe Nucleotide
+toNucleotide 'A' = Just A
+toNucleotide 'C' = Just C
+toNucleotide 'G' = Just G
+toNucleotide 'T' = Just T
+toNucleotide _ = Nothing
+
+frequency :: (Ord a) => [a] -> [(a, Int)]
+frequency xs = Map.toList (Map.fromListWith (+) [(x, 1) | x <- xs])
