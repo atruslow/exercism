f24377c7127ead0b682807c89aa3af3a04df75b7
Alex Truslow <atruslow@mercury.com>
2024-03-08 13:55:04 -0500

    refactoring

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,16 +1,23 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Anagram (anagramsFor) where
 
+import Data.Char (toLower)
+import Data.List (sort)
 import Data.Set (Set)
 import qualified Data.Set as Set
-import Data.List (sort)
-import Data.Char (toLower)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = [ inputStr | (inputStr, _, _) <- matchingSets ]
-    where valueSet = Set.fromList (lower xs)
-          valueSort = (sort $ lower xs)
-          inputSets = [(c, Set.fromList (lower c), (sort (lower c))) | c <- xss]
-          matchingSets = filter (\(inputStr, charSet, sortStr) -> valueSet == charSet && valueSort == sortStr && not ( lower xs ==  lower inputStr )) inputSets
+anagramsFor xs xss = [inputStr | (inputStr, _, _) <- matchingSets]
+ where
+  matchingSets = filter isMatch xs y inputSets
+  inputSets = [(c, Set.fromList (lower c), sort (lower c)) | c <- xss]
+
+isMatch :: String -> (String, Set Char, String) -> Bool
+isMatch xs (inputStr, charSet, sortStr) = valueSet == charSet && valueSort == sortStr && lower xs /= lower inputStr
+ where
+  valueSort = sort $ lower xs
+  valueSet = Set.fromList (lower xs)
 
 lower :: String -> String
 lower xs = [toLower x | x <- xs]
