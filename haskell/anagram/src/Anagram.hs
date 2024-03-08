b1996020a8eab72736c2154dcfc06b4ca978f1b7
Alex Truslow <atruslow@mercury.com>
2024-03-08 13:10:29 -0500

    Anagram Haskell

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,4 +1,16 @@
 module Anagram (anagramsFor) where
 
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Data.List (sort)
+import Data.Char (toLower)
+
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor xs xss = [ inputStr | (inputStr, _, _) <- matchingSets ]
+    where valueSet = Set.fromList (lower xs)
+          valueSort = (sort $ lower xs)
+          inputSets = [(c, Set.fromList (lower c), (sort (lower c))) | c <- xss]
+          matchingSets = filter (\(inputStr, charSet, sortStr) -> valueSet == charSet && valueSort == sortStr && not ( lower xs ==  lower inputStr )) inputSets
+
+lower :: String -> String
+lower xs = [toLower x | x <- xs]
